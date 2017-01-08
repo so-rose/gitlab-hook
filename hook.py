4c7b8625618c7ad15d9a8ed2e2a5835bc71778c9
Sofus Rose <sofus@sofusrose.com>
2017-01-08 18:00:34 -0500

    Added requirements.txt

--- hook.py
+++ hook.py
@@ -1,12 +1,13 @@
 from flask import Flask, request
 import json
+import sys
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
    data = json.loads(request.data)
-   print("New commit by: {}".format(data['commits'][0]['author']['name']))
+   print("New commit by: {}".format(data['commits'][0]['author']['name']), file=sys.stderr)
    return "OK"
 
 if __name__ == '__main__':
