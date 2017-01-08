616cfabbc7f47436e2547d8921f6a768c5668a76
Sofus Rose <sofus@sofusrose.com>
2017-01-08 18:06:49 -0500

    Alternate method of getting json data.

--- hook.py
+++ hook.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
-   data = json.loads(request.data)
-   print("New commit by: {}".format(data['commits'][0]['author']['name']), file=sys.stderr)
-   return "OK"
+	data = request.get_json()
+	#~ data = json.loads(request.data)
+	print("New commit by: {}".format(data['commits'][0]['author']['name']), file=sys.stderr)
+	print("Header: ", request.headers)
+	return "OK"
 
 if __name__ == '__main__':
-   app.run(port=7080)
+	app.run(port=7080)
