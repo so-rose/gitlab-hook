5f2b2300c364c24362197e5de19f40ba4ecc70e2
Sofus Rose <sofus@sofusrose.com>
2017-01-08 18:48:45 -0500

    Added gitpython logic. First test.

--- hook.py
+++ hook.py
@@ -1,16 +1,45 @@
 from flask import Flask, request
+from git import Repo
+
 import json
 import sys
+import os, os.path
+
+github_user = 'so-rose'
+TOKEN = 'siueyrhg87w35ht8jmwfy7h5g'
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def foo():
 	data = request.get_json()
-	#~ data = json.loads(request.data)
+
 	print("New commit by: {}".format(data['commits'][0]['author']['name']), file=sys.stderr)
 	print("Header: ", request.headers)
-	return "OK"
+	
+	if request.headers['X-Gitlab-Token'] == TOKEN :
+		proj_name = data['project']['name']
+		github_link = 'git@github.com/{0}/{1}.git'.format(github_user, proj_name)
+		gitlab_link = data['project']['git_http_url']
+		repo_path = "/opt/git-repos/{}".format(proj_name)
+		
+		#Initialize & prepare repo object.
+		repo = Repo(repo_path) if os.path.isdir(repo_path) else Repo.clone_from(gitlab_link, repo_path)
+		
+		#Get remotes, pull and push.
+		github = repo.create_remote('github', github_link)
+		gitlab = repo.remotes.origin
+		
+		for branch in repo.branches :
+			repo.git.checkout('HEAD', b=branch)
+			gitlab.pull()
+			github.push()
+			
+		print github.fetch()
+		
+		return "OK"
+	else :
+		return 403
 
 if __name__ == '__main__':
 	app.run(port=7080)
